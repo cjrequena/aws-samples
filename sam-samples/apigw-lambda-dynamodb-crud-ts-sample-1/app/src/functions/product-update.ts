4c1cb4fae4af4a4935c4ed437bcc58757e3d6a77
cjrequena <cjrequena+git@protonmail.com>
2023-08-09 15:41:30 +0200

    --

--- sam-samples/apigw-lambda-dynamodb-crud-ts-sample-1/app/src/functions/product-update.ts
+++ sam-samples/apigw-lambda-dynamodb-crud-ts-sample-1/app/src/functions/product-update.ts
@@ -23,7 +23,7 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
         return {
           statusCode: 200,
           body: JSON.stringify({
-            message: "CALLED RETRIEVE BY ID",
+            message: "CALLED CREATE",
           }),
         };
         break;
